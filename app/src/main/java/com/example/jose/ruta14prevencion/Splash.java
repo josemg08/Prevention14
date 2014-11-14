f3ac51fae6a39589d3cb0948356b0e776d21b7b8
Jose Maria <jose@tekgenesis.com>
2014-11-14 17:09:13 -0300

    Add splash screen

--- app/src/main/java/com/example/jose/ruta14prevencion/Splash.java
+++ app/src/main/java/com/example/jose/ruta14prevencion/Splash.java
@@ -1,7 +1,45 @@
 package com.example.jose.ruta14prevencion;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 /**
  * Created by jose on 11/11/14.
  */
-public class Splash {
+public class Splash extends Activity implements Runnable{
+
+    Thread mThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash);
+
+        mThread = new Thread(this);
+
+        mThread.start();
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            Thread.sleep(5000);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            startActivity(new Intent(Splash.this, MapsActivity.class));
+
+            finish();
+        }
+    }
+
 }
